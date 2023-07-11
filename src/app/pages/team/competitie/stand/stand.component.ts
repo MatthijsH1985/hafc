70b1c43819f93499ab8d1eab66c2c580bb16b642
hondebrink <matthijs.hondebrink@topicus.nl>
2023-07-11 20:29:05 +0200

    Wip

--- src/app/pages/team/competitie/stand/stand.component.ts
+++ src/app/pages/team/competitie/stand/stand.component.ts
@@ -27,13 +27,15 @@ export class StandComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.viewportScroller.scrollToPosition([0,0]);
-    this.metaService.updateMetaTag(this.router.url, 'De stand van de Eredivisie');
     this.gtmService.startTrackingTags();
     this.title.setTitle('Stand - HAFC.nl')
     this.rankingSub = this.standingsService.getStandings(this.currentSeasonId).subscribe({
       next: (data: any) => {
         this.loading = false;
         this.ranking = data.data;
+        if (!this.compact) {
+          this.metaService.updateMetaTag(this.router.url, 'De stand van de Eredivisie');
+        }
         if(this.compact) {
           const club = 'Heracles Almelo';
           this.ranking = this.selectieRijen(this.ranking, club);
