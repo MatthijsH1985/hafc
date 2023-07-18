7504d045663aec2c724c9bed07fd6bef422cd269
hondebrink <matthijs.hondebrink@topicus.nl>
2023-07-18 07:38:22 +0200

    Wip

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -5,7 +5,7 @@ import {Config} from '../model/config';
 @Injectable()
 export class ConfigService {
   config: Config = {
-    "apiEndpoint": "http://localhost:4200/api",
+    "apiEndpoint": "https://www.hafc.nl/api",
     "authEndPoint": "https://backend.nl/wp-json/jwt-auth/v1",
     "apiEndPointDev": "http://hafc.site.development:8080/wp-json"
   };
