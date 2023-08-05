661ef9d4e38972d3feba28268cb5bc9902b1f0bf
hondebrink <matthijs.hondebrink@topicus.nl>
2023-08-06 00:07:03 +0200

    Wip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiUrl: 'https://backend.hafc.nl/wp-json/wp/v2',
   loginUrl: 'https://backend.hafc.nl/wp-json/jwt-auth/v1/token',
-  userServiceUrl: 'userservice',
+  userServiceUrl: 'https://backend.hafc.nl/wp-json/userservice',
   recaptcha: {
     siteKey: '6LdyyrcUAAAAAB81RMMORr90yBEMcYN_omdANSEe',
   },
