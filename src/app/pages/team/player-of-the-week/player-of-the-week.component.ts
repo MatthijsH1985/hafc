5a00716348be38cd2dbc6fa64a707926732fea7c
hondebrink <matthijs.hondebrink@topicus.nl>
2023-09-29 10:48:43 +0200

    Wip

--- src/app/pages/team/player-of-the-week/player-of-the-week.component.ts
+++ src/app/pages/team/player-of-the-week/player-of-the-week.component.ts
@@ -1,27 +1,40 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {PlayersService} from "../../../services/players.service";
 import {Subscription} from "rxjs";
-
+interface PlayerOfTheWeek {
+  ID: number;
+  playerName: string;
+  slug: string;
+  image: string;
+}
 @Component({
   selector: 'app-player-of-the-week',
   templateUrl: './player-of-the-week.component.html',
   styleUrls: ['./player-of-the-week.component.scss']
 })
 export class PlayerOfTheWeekComponent implements OnInit, OnDestroy{
-  playerOfTheWeek: any;
+  playerOfTheWeek: PlayerOfTheWeek | undefined;
   playerOfTheWeekSub: Subscription = new Subscription();
+
   constructor(private playersService: PlayersService) {
   }
   ngOnInit() {
     this.playerOfTheWeekSub = this.playersService.getPlayerOfTheWeek().subscribe({
       next: (player: any) => {
-        this.playerOfTheWeek = player[0];
-        console.log(this.playerOfTheWeek)
+        this.generatePlayerOfTheWeek(player)
       },
       error: (error) => {
         return error;
       }
     })
+  }
+  generatePlayerOfTheWeek(player: any) {
+    this.playerOfTheWeek = {
+      ID: player[0].acf.speler_van_de_week?.ID,
+      playerName: player[0]?.acf?.speler_van_de_week?.post_title,
+      slug: player[0]?.acf?.speler_van_de_week?.post_name,
+      image: player[0]?.better_featured_image?.source_url
+    }
   }
   ngOnDestroy() {
     this.playerOfTheWeekSub.unsubscribe();
