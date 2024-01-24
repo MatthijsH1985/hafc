6c7004cb9ffae921dd7defb1caefc87d3c419a3b
hondebrink <matthijs.hondebrink@topicus.nl>
2024-01-24 19:05:23 +0100

    Wip

--- src/app/shop/services/products.service.ts
+++ src/app/shop/services/products.service.ts
@@ -12,7 +12,7 @@ export class ProductsService {
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json;charset=UTF-8',
-      'Authorization': 'Basic ' + btoa('ck_d3f494e8666a090e62a2edb9f54d25cf97a28dd1:cs_1f35f25e3ce306013ab9a6e26a080e3bef71e1a1')
+      'Authorization': 'Basic ' + btoa('ck_42bfce6f7023aefda6fdfb791424a911605f1217:cs_6bf2ce3713f644aad9a6e458e928508a25fbe2fb')
     })
   };
 
