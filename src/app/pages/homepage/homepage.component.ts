c5d79b6c765c0661514cb10f179f2ea5fa22af77
hondebrink <matthijs.hondebrink@topicus.nl>
2024-06-12 19:40:16 +0200

    Wip

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,11 +1,10 @@
-import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
+import {ChangeDetectorRef, Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {Title} from "@angular/platform-browser";
 import {isPlatformBrowser, ViewportScroller} from "@angular/common";
 import {NewssliderComponent} from "../../news/newsslider/newsslider.component";
 import {MetaService} from "../../core/services/meta.service";
 import {LoadingIndicatorService} from "../../core/shared/loading-indicator/loading-indicator.service";
-import {PlayersService} from "../../services/players.service";
 import {Link} from "../../core/model/link.interface";
 import {ToastrService} from "ngx-toastr";
 import {AuthService} from '../../services/auth/auth-service';
@@ -22,11 +21,17 @@ export class HomepageComponent implements OnInit {
   links: Link[] = [];
   reactiesString = 'reacties';
   headlines: any;
+  targetWidth = 76; // The target percentage width
+  totalDuration = 2000; // Total duration of the animation and counting in milliseconds
+  animationDuration = 2000; // Duration of the animation in milliseconds
+  countingInterval = 10; // Interval for counting in milliseconds
+  currentWidth = 0;
+  currentPercentage = 0;
   constructor(
               private titleService: Title,
-              private playersService: PlayersService,
               private viewportScroller: ViewportScroller,
               private metaService: MetaService,
+              private cdr: ChangeDetectorRef,
               private toastr:ToastrService,
               private authService: AuthService,
               private loadingIndicatorService: LoadingIndicatorService,
@@ -66,7 +71,8 @@ export class HomepageComponent implements OnInit {
       let donateMessage = '<p>Kun jij een kleine bijdrage missen voor het komende jaar? HAFC wil jullie ook dit jaar weer voorzien van het laatste nieuws!</p> <a class="block mt-2 -ml-2 underline text-black p-2" href="https://www.hafc.nl/doneer" target="_blank">Ja, ik steun HAFC met een eenmalige donatie</a>';
       // this.onShowDonateMessage(donateTitle, donateMessage);
     }
-
+    this.animateProgressBar();
+    this.startCounting();
     this.latestComments = this.route.snapshot.data['latestComments'];
     this.metaService.setMetaTag('https://www.hafc.nl', 'HAFC.nl is de grootste Heracles community voor en door supporters. Volg hier het laatste nieuws en blijf op de hoogte', 'https://backend.hafc.nl/wp-content/uploads/2023/05/nac-heracles.jpg');
   }
@@ -84,6 +90,60 @@ export class HomepageComponent implements OnInit {
           timeOut: 10000,
           closeButton:true
         })
+    }
+  }
+
+  animateProgressBar(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      setTimeout(() => {
+        const interval = 20; // Update interval in milliseconds
+        const animationSteps = this.animationDuration / interval;
+        let step = 0;
+
+        const updateProgress = () => {
+          step++;
+          const progress = step / animationSteps;
+          this.currentWidth = progress * this.targetWidth;
+
+          this.cdr.detectChanges(); // Manually trigger change detection
+
+          if (step < animationSteps) {
+            setTimeout(updateProgress, interval);
+          } else {
+            // Ensure the final state is exactly the target
+            this.currentWidth = this.targetWidth;
+            this.cdr.detectChanges(); // Final change detection
+          }
+        };
+
+        setTimeout(updateProgress, interval);
+      }, 2000)
+    }
+  }
+
+  startCounting(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      setTimeout(() => {
+        const countingSteps = this.totalDuration / this.countingInterval;
+        let step = 0;
+
+        const updateCount = () => {
+          step++;
+          const progress = step / countingSteps;
+          this.currentPercentage = Math.round(progress * this.targetWidth);
+
+          this.cdr.detectChanges(); // Manually trigger change detection
+
+          if (step < countingSteps) {
+            setTimeout(updateCount, this.countingInterval);
+          } else {
+            // Ensure the final state is exactly the target
+            this.currentPercentage = this.targetWidth;
+            this.cdr.detectChanges(); // Final change detection
+          }
+        };
+        setTimeout(updateCount, this.countingInterval);
+      }, 2000)
     }
   }
 
