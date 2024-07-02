383b512f22f283177e11198ead49b58745026bdc
hondebrink <matthijs.hondebrink@topicus.nl>
2024-07-02 22:12:04 +0200

    Wip

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -2,11 +2,15 @@ import {mergeApplicationConfig, ApplicationConfig, importProvidersFrom} from '@a
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
 import {provideHttpClient, withFetch} from '@angular/common/http';
+import {ConfigService} from './core/services/config.service';
+import {PostsService} from './news/services/posts.service';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    ConfigService,
+    PostsService
   ]
 };
 
