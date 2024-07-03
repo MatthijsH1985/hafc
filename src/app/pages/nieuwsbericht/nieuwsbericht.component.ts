891ba34b07664c0fd72491ea1398f3fde37969df
hondebrink <matthijs.hondebrink@topicus.nl>
2024-07-03 16:44:44 +0200

    Test

--- src/app/pages/nieuwsbericht/nieuwsbericht.component.ts
+++ src/app/pages/nieuwsbericht/nieuwsbericht.component.ts
@@ -63,8 +63,8 @@ export class NieuwsberichtComponent implements OnInit, OnDestroy, AfterViewInit 
     private loadingIndicatorService: LoadingIndicatorService,
     @Inject(PLATFORM_ID) private platformId: object
   ) {
-    this.post = this.route.snapshot.data['post'];
-    this.metaService.updateMetaTag(this.post);
+    // this.post = this.route.snapshot.data['post'];
+    // this.metaService.updateMetaTag(this.post);
   }
 
   ngOnInit() {
